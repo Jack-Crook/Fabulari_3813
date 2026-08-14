fe2ee78aa8433615180f39f925a3c62cfc4d0b3f
Jack-Crook <crookjack96@gmail.com>
2026-08-14 13:13:10 +1000

    added login route, login page works end to end

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Register } from './register/register'; // the component to show on /register
+import { Login } from './login/login';
 
 export const routes: Routes = [
   { path: 'register', component: Register },      // localhost:4200/register -> Register component
-  { path: '', redirectTo: 'register', pathMatch: 'full' }, // temporary landing page until login exists
+  { path: 'login', component: Login},
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // temporary landing page until login exists
 ];
