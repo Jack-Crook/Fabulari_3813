69fc6aabd962c93e324156d67e9b6f0e4840d313
Jack-Crook <crookjack96@gmail.com>
2026-08-14 11:32:04 +1000

    wired up register page, added route, router outlet and form and submit logic

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Register } from './register/register'; // the component to show on /register
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: Register },      // localhost:4200/register -> Register component
+  { path: '', redirectTo: 'register', pathMatch: 'full' }, // temporary landing page until login exists
+];
