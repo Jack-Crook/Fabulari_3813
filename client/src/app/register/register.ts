f158e4ff621d1aeac96afbdc69383cd14e4aa82b
Jack-Crook <crookjack96@gmail.com>
2026-08-18 12:53:47 +1000

    finished styling register

--- client/src/app/register/register.ts
+++ client/src/app/register/register.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Auth } from '../auth';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
